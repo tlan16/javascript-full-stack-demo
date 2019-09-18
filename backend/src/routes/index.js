6d5e273c431e5dcd52c8bceae1317dfeb55037b3
Frank Lan <frank.lan@balanceinternet.com.au>
2019-09-18 14:43:46 +1000

    replace old backend code with nest

--- backend/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const healthCheck = require('./healthCheck');
-const customer = require('./customer');
-
-module.exports = {
-  healthCheck: healthCheck(),
-  customer: customer(),
-};
